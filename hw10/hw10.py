ad51e08b3af217253fe5db3b0a6c9a99ce3e0043
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-11-11 23:23:38 -0500

    Update hw10.py

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -3,23 +3,28 @@
 def is_anagram(s1, s2):
   x = 0
   while x < len(s1 + s2):
-	for c in b:
-		lob = b.lower()
-		lobs = lob.strip()
-		lobst = lobs.strip('"')
-		lobstr = lobst.strip("-")
-		lobstri = lobstr.strip(".")
-		lobstrin = lobstri.strip(" ")
-		loa = a.lower()
-		loas = loa.strip()
-		loast = loas.strip('"')
-		loastr = loast.strip("-")
-		loastri = loastr.strip(".")
-		loastrin = loastri.strip(" ")
-	x = x + 1
+    for c in b:
+	    lob = b.lower()
+	    lobs = lob.strip()
+	    lobst = lobs.strip('"')
+	    lobstr = lobst.strip("-")
+	    lobstri = lobstr.strip(".")
+	    lobstrin = lobstri.strip(" ")
+	    loa = a.lower()
+	    loas = loa.strip()
+		  loast = loas.strip('"')
+		  loastr = loast.strip("-")
+		  loastri = loastr.strip(".")
+		  loastrin = loastri.strip(" ")
+	  x = x + 1
   bl = sorted(list("".join(lobstrin.split())))
   al = sorted(list("".join(loastrin.split())))
   if bl == al:
 	  return "anagram"
   else:
-	  return "not anagram" 
+	  return "not anagram"
+  
+  if __name__ == "__main__":
+    s1 = input("Enter a string: \n")
+    s2 = input("Enter a string: \n")
+    is_anagram(s1, s2)
