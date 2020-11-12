45143c61292fc3d06e0b129466cf0d62918decbf
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-11-11 23:15:14 -0500

    Update hw10.py

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -1,4 +1,24 @@
-# Author: Yanling Wang yuw17@psu.edu
-# GitHub ID: yanlingpsu
+# Author: Carter Monroe cam7002@psu.edu
+# GitHub ID: Strik3Up
 def is_anagram(s1, s2):
-  return True
+  x = 0
+  while x < len(s1 + s2):
+	  lob = s2.lower()
+	  lobs = lob.strip()
+	  lobst = lobs.strip('"')
+	  lobstr = lobst.strip("-")
+	  lobstri = lobstr.strip(".")
+	  lobstrin = lobstri.strip(" ")
+	  loa = s1.lower()
+	  loas = loa.strip()
+	  loast = loas.strip('"')
+	  loastr = loast.strip("-")
+	  loastri = loastr.strip(".")
+	  loastrin = loastri.strip(" ")
+	  x = x + 1
+  bl = sorted(list("".join(lobstrin.split())))
+  al = sorted(list("".join(loastrin.split())))
+  if bl == al:
+	  return "anagram"
+  else:
+	  return "not anagram" 
