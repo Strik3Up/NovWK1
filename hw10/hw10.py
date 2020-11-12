d0d17bb685e20196baa59591bba7035c9f83f982
Strik3Up <70175372+Strik3Up@users.noreply.github.com>
2020-11-11 23:29:26 -0500

    Update hw10.py

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -2,21 +2,20 @@
 # GitHub ID: Strik3Up
 def is_anagram(s1, s2):
   x = 0
-  c = 0
   while x < len(s1 + s2):
     for c in b:
-	    lob = b.lower()
-	    lobs = lob.strip()
-	    lobst = lobs.strip('"')
-	    lobstr = lobst.strip("-")
-	    lobstri = lobstr.strip(".")
-	    lobstrin = lobstri.strip(" ")
-	    loa = a.lower()
-	    loas = loa.strip()
-		  loast = loas.strip('"')
-		  loastr = loast.strip("-")
-		  loastri = loastr.strip(".")
-		  loastrin = loastri.strip(" ")
+      lob = b.lower()
+      lobs = lob.strip()
+      lobst = lobs.strip('"')
+      lobstr = lobst.strip("-")
+      lobstri = lobstr.strip(".")
+      lobstrin = lobstri.strip(" ")
+      loa = a.lower()
+      loas = loa.strip()
+      loast = loas.strip('"')
+      loastr = loast.strip("-")
+      loastri = loastr.strip(".")
+      loastrin = loastri.strip(" ")
 	  x = x + 1
   bl = sorted(list("".join(lobstrin.split())))
   al = sorted(list("".join(loastrin.split())))
